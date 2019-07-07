8b827834d5119533de64d7a35acaa5c02c2e28c5
Zach McGonigal <zach.mcgonigal@gmail.com>
2019-07-07 18:09:01 -0400

    wiring up the account view to the url

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Transaction, Account, Payee, Category } from '../model';
-import {MatTableDataSource} from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { Transaction, Account } from '../model';
+import { MatTableDataSource } from '@angular/material/table';
 import { AccountService, TransactionService } from '../services';
-
-const TRANSACTIONS: Transaction[] = [
-  
-]
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   templateUrl: './account.component.html',
@@ -14,15 +12,22 @@ const TRANSACTIONS: Transaction[] = [
 export class AccountComponent implements OnInit{
   displayedColumns: string[] = ['day', 'payee', 'category', 'memo', 'outflow', 'inflow'];
   dataSource = new MatTableDataSource<Transaction>([]);
+  private account: Account;
 
   constructor(
+    private route: ActivatedRoute,
     private $account: AccountService,
     private $transaction: TransactionService
   ) {}
 
   ngOnInit() {
-    this.$transaction.getTransactionsForAccount(new Account({id: '1'})).subscribe(transactions => {
-      this.dataSource.connect().next(transactions);
+    this.route.paramMap.pipe(
+      switchMap(params => this.$account.getAccountById(params.get('account_id')))
+    ).subscribe(account => {
+      this.account = account;
+      this.$transaction.getTransactionsForAccount(account).subscribe(transactions => {
+        this.dataSource.connect().next(transactions);
+      });
     });
   }
 }
